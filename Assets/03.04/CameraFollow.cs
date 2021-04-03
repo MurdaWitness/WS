35957ef49673414b8dd43c7f2ffc0829759675b9
MurdaWitness <gavrishin.a@yandex.ru>
2021-04-03 15:46:58 +0000

    Hz

---
+++ Assets/03.04/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform targetFollow;
+    public Transform targetLook;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Follow();
+        transform.LookAt(targetLook);
+        if (Input.GetKey(KeyCode.F);
+    }
+
+    public void Follow()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetFollow.position, speed);
+    }
+}
---
+++ Assets/03.04/Play.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    public float speed;
+    public float stopDistance = 2f;
+    public Transform target;
+    public bool useLerp;
+    public bool useTowards;
+
+    void Update()
+    {
+        var t = new Vector3(target.position.x, transform.position.y, target.position.z);
+        if (Vector3.Distance(transform.position, t) >= stopDistance)
+        {
+            if (useLerp) MoveByLerp(t);
+            if (useTowards) MoveByTowards(t);
+            transform.LookAt(t);
+        }
+    }
+
+    public void MoveByLerp(Vector3 t)
+    {
+        transform.position = Vector3.Lerp(transform.position, t, speed * Time.deltaTime);
+    }
+
+    public void MoveByTowards(Vector3 t)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, t, speed * Time.deltaTime);
+    }
+
+}
